5e48da332b102bc73def43cc2f30740170a7e5c2
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-04-29 12:53:16 +1000

    add version; let's go straight to v1.0.0

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"path/filepath"
 )
 
+const Version = "v1.0.0"
+
 func main() {
+	fmt.Printf("OpenBinary %s\n\n", Version)
 	if len(os.Args) <= 1 {
 		fmt.Printf("Please specify file to open with %s\n", os.Args[0])
 		os.Exit(1)
