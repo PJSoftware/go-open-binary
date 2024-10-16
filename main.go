d28215feb3f0479cf91093252e9781f8ac4a16c4
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-10-16 12:02:19 +1000

    v1.0.1 - detect case where file does not exist

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
-const Version = "v1.0.0"
+const Version = "v1.0.1"
 
 func main() {
 	fmt.Printf("OpenBinary %s\n\n", Version)
@@ -17,6 +18,7 @@ func main() {
 	}
 
 	for _, bf := range os.Args[1:] {
+		fmt.Printf("Opening '%s'\n", bf)
 		OpenWithPowershell(bf)
 	}
 }
@@ -24,7 +26,13 @@ func main() {
 func OpenWithPowershell(bf string) {
 	bfp, err := filepath.Abs(bf)
 	if err != nil {
-		fmt.Printf("Could not find '%s'; skipping\n", bf)
+		fmt.Printf("> Could not identify '%s'; skipping\n", bf)
+		return
+	}
+
+	if _, err := os.Stat(bfp); errors.Is(err, os.ErrNotExist) {
+		fmt.Printf("> File '%s' not found; skipping\n", bfp)
+		return
 	}
 
 	fmt.Printf("Opening '%s':\n", bfp)
